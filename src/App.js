e7f783a57bd577d944c2cb898b8859b14b679f96
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-01-14 16:15:59 +0330

    coding routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 import Header from './Components/Header/Header';
-import './App.css';
 import Sidebar from "./Components/Sidebar/Sidebar";
+import {useRoutes} from 'react-router-dom'
+import './App.css';
+import routes from "./routes";
 
 function App() {
+
+  const router = useRoutes(routes)
+
   return (
     <>
       <Header />
@@ -13,6 +18,8 @@ function App() {
         <main class="main">
           <div class="row justify-content-between mx-0">
             <Sidebar />
+
+            {router}
           </div>
         </main>
       </div>
