ef052be14d4069ecea7eedb70fba0de4274652c2
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-01-14 16:09:29 +0330

    coding Sidebar Component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from "react";
 
 import Header from './Components/Header/Header';
 import './App.css';
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+
+      <div class="container px-0">
+        <main class="main">
+          <div class="row justify-content-between mx-0">
+            <Sidebar />
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
 
