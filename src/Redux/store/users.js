e0e25933b52d743e6f5cbad95c2cf7d0d68c5103
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-01-17 16:50:44 +0330

    fixed bug from users.js store

--- src/Redux/store/users.js
+++ src/Redux/store/users.js
@@ -1,24 +1,22 @@
-import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 
 export const getUsersFromServer = createAsyncThunk(
     'users/getUsersFromServer',
     async (url) => {
         return fetch(url)
-        .then(res => res.json())
-        .then(data => data)
+            .then(res => res.json())
+            .then(data => data)
     }
 )
 
 const slice = createSlice({
-    name :'getUsersFromServer',
-    initialState : [],
-    reducers : {},
-    extraReducers :builder => {
-        builder.addCase(getUsersFromServer.fulfilled,(state,action) => {
-            state.push(...action.payload)
-        })
+    name: 'getUsersFromServer',
+    initialState: [],
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(getUsersFromServer.fulfilled, (state, action) => action.payload)
     }
 })
 
-export default slice.reducer ;
+export default slice.reducer;
